273286b56c2775b7a87f57c2bc0a37b74218fd08
Sadee <mohammadsadee24@gmail.com>
2021-04-08 13:17:41 +0600

    minor changes

--- snake.js
+++ snake.js
@@ -1,6 +1,6 @@
 import { getInputDirection } from "./input.js";
 
-export const SNAKE_SPEED = Number(prompt("Enter the speed"));
+export const SNAKE_SPEED = 10;
 const snakeBody = [{ x: 20, y: 20 }];
 let newSegments = 0;
 
